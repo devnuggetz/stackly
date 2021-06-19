17dd3d51306ac85ad07bc9795585782293a5ded4
shuvamk <kshuvam564@gmail.com>
2021-06-20 02:00:32 +0530

    Added language cards

--- components/home/trending-technologies/index.js
+++ components/home/trending-technologies/index.js
@@ -1,14 +1,21 @@
 import React from "react";
 import styles from "./trending-technologies.module.css";
+import { languageData } from "../../../data/language";
+import LanguageCard from "./language-card";
 
 function TrendingTechnologies() {
+  const data = languageData;
   return (
     <div className={styles.trendContainer}>
       <div className={styles.trendSection}>
         <lable className={styles.trendHeading}>Trending Technologies</lable>
       </div>
       <div className={styles.trendSection}>
-        <div className={styles.trendCards}></div>
+        <div className={styles.trendCards}>
+          {data.map((item) => {
+            return <LanguageCard language={item} />;
+          })}
+        </div>
       </div>
     </div>
   );
--- components/home/trending-technologies/language-card.js
+++ components/home/trending-technologies/language-card.js
@@ -1,7 +1,13 @@
 import React from "react";
-
-function LanguageCard() {
-  return <div>This is Language Card</div>;
+import styles from "./language-card.module.css";
+function LanguageCard({ language }) {
+  return (
+    <div className={styles.cardContainer}>
+      <label className={styles.cardTitle}>{language.name}</label>
+      <div className={styles.logoContainer}>{language.svg("#6563FF")}</div>
+      <label className={styles.cardSubtitle}>Used by: {language.count}</label>
+    </div>
+  );
 }
 
 export default LanguageCard;
